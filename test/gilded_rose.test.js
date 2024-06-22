77a814cbc32c624c0af00f0ffc71a7110d52c2ed
AndyFerrie <93442058+AndyFerrie@users.noreply.github.com>
2024-06-22 10:10:34 +0100

    Adds tests for non legandary items

--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -1,9 +1,21 @@
 const {Shop, Item} = require("../src/gilded_rose");
 
 describe("Gilded Rose", function() {
-  it("should foo", function() {
-    const gildedRose = new Shop([new Item("foo", 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("fixme");
-  });
-});
+    describe("Given a non-legendary item", function() {
+        test("When the sell by date has not passed, then `Quality` decreases by 1", function() {
+        const gildedRose = new Shop([new Item("non-legendary item", 2, 4)]);
+        const items = gildedRose.updateQualityForMultipleDays(1)
+        expect(items[0].quality).toBe(3);
+        });
+        test("When the sell by date has passed, then `Quality` degrades twice as fast", function() {
+            const gildedRose = new Shop([new Item("non-legendary item", 1, 4)]);
+            const items = gildedRose.updateQualityForMultipleDays(2)
+            expect(items[0].quality).toBe(1);
+        });
+        test("When the sell by date has passed, then `Quality` cannot be negative", function() {
+            const gildedRose = new Shop([new Item("non-legendary item", 1, 0)]);
+            const items = gildedRose.updateQualityForMultipleDays(2)
+            expect(items[0].quality).toBe(0);
+        });
+    });
+})
